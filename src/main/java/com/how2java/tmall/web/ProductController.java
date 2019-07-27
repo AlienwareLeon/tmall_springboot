e128af7fd0f792db0436dbf21738905f8b65a9bd
Leon <liuyujie_leon@163.com>
2019-07-28 01:22:38 +0800

    tmall_springboot

--- src/main/java/com/how2java/tmall/web/ProductController.java
+++ src/main/java/com/how2java/tmall/web/ProductController.java
@@ -5,10 +5,10 @@ import com.how2java.tmall.service.CategoryService;
 import com.how2java.tmall.service.ProductService;
 import com.how2java.tmall.util.Page4Navigator;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.Date;
 
 @RestController
 public class ProductController {
@@ -18,5 +18,36 @@ public class ProductController {
     CategoryService categoryService;
 
     @GetMapping("/categories/{cid}/products")
-    public Page4Navigator<Product> list(@PathVariable("cid") int cid, @RequestParam(value = "start", defaultValue = "0") int start)
+    public Page4Navigator<Product> list(@PathVariable("cid") int cid, @RequestParam(value = "start", defaultValue = "0") int start, @RequestParam(value = "size", defaultValue = "5") int size)
+        throws Exception{
+        start = start<0?0:start;
+        Page4Navigator<Product> page = productService.list(cid, start, size, 5);
+
+        return page;
+    }
+
+    @GetMapping("/products/{id}")
+    public Product get(@PathVariable("id") int id) throws Exception {
+        Product bean = productService.get(id);
+        return bean;
+    }
+
+    @PostMapping("/products")
+    public Object add(@RequestBody Product bean) throws Exception {
+        bean.setCreateDate(new Date());
+        productService.add(bean);
+        return bean;
+    }
+
+    @DeleteMapping("/products/{id}")
+    public String delete(@PathVariable("id") int id, HttpServletRequest request) throws Exception {
+        productService.delete(id);
+        return null;
+    }
+
+    @PutMapping("/products")
+    public Object update(@RequestBody Product bean) throws Exception {
+        productService.update(bean);
+        return bean;
+    }
 }
