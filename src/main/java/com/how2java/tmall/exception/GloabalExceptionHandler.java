bb367a7e0063281ca3f25f9b712a1be9bc8a7caf
Alienwareleon <alienwareleon@gmail.com>
2019-08-16 05:33:35 +0800

    tmall_springboot

--- src/main/java/com/how2java/tmall/exception/GloabalExceptionHandler.java
+++ src/main/java/com/how2java/tmall/exception/GloabalExceptionHandler.java
@@ -10,10 +10,10 @@ import javax.servlet.http.HttpServletRequest;
 @ControllerAdvice
 public class GloabalExceptionHandler {
     @ExceptionHandler(value = Exception.class)
-    public String defaultErrorHandler(HttpServletRequest request, Exception e) throws  Exception{
+    public String defaultErrorHandler(HttpServletRequest req, Exception e) throws  Exception{
         e.printStackTrace();
         Class constraintViolationException = Class.forName("org.hibernate.exception.ConstraintViolationException");
-        if (null != e.getCause() && constraintViolationException == e.getCause().getClass()){
+        if (null!=e.getCause() && constraintViolationException==e.getCause().getClass()){
             return "违反约束，多半是外键约束";
         }
         return e.getMessage();
